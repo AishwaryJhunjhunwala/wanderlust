c27ff33b4e35f1e6422a28ba35a76a5cf374c1f1
AishwaryJhunjhunwala <aishwaryjjw@gmail.com>
2025-06-24 13:24:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use((req,res,next)=>{
 });
 
 app.get("/",(req,res)=>{
-  res.redirect("/signup"):});
+  res.redirect("/signup")});
 
 app.use("/listings",listingsRouter);
 app.use("/listings/:id/reviews",reviewRouter);
